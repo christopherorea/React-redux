db4d2a54de328f617587b711c2a2b62dfe155811
christopherorea <c28_09_98@hotmail.com>
2020-09-11 01:06:33 -0500

    Updates

---
+++ precache-manifest.563b7d1fa8d3ce801594af05147d5162.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccb99651fdbedf4fab09163f642135b0",
+    "url": "/React-redux/index.html"
+  },
+  {
+    "revision": "2f47c41fdd7c648c8f0a",
+    "url": "/React-redux/static/js/2.848085ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-redux/static/js/2.848085ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c218ba3a3c75d64b40e9",
+    "url": "/React-redux/static/js/main.628a4136.chunk.js"
+  },
+  {
+    "revision": "bbbfd326894f6aa2a00f",
+    "url": "/React-redux/static/js/runtime-main.60195355.js"
+  }
+]);
